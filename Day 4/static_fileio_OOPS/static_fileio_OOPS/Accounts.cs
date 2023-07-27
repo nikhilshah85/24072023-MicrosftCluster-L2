2261290a2cd3dc1e49f5de05458a78606066293c
Nikhil <nikhilshah.net@hotmail.com>
2023-07-27 15:51:36 +0530

    Day 4

---
+++ Day 4/static_fileio_OOPS/static_fileio_OOPS/Accounts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_fileio_OOPS
+{
+    enum AccountTypes
+    {
+        Savings, Current, PF
+    }
+
+    internal class Accounts
+    {
+
+       // static int v_autoAccountNumber = 100; //default value is 0 even if you do not instantiate it
+        #region Properties
+        public int AccNo { get; set; }
+        public string AccName { get; set; }
+        public string AccType { get; set; }
+        public double AccBalance { get; set; }
+        public bool AccIsActive { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Accounts(string p_accName, AccountTypes p_accType, double p_accBalance, bool p_accIsActive)
+        {
+            //you can do validations here , eg. check name length or accbalance to be minimum while opening account
+            //throw exception if not valid
+            AccName = p_accName;
+            AccType = p_accType.ToString();
+            AccBalance = p_accBalance;
+            AccIsActive = p_accIsActive;
+
+            #region read the number
+            FileStream file_accountNo = new FileStream("autoaccNo.txt", FileMode.Open, FileAccess.Read);
+            StreamReader readNo = new StreamReader(file_accountNo);
+            
+            int number =Convert.ToInt32(readNo.ReadLine());
+            number = number + 1;
+            AccNo = number;
+            readNo.Close();
+            file_accountNo.Close();
+            #endregion
+
+            #region Save the number back to file
+
+            FileStream write_accountNo = new FileStream("autoaccNo.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter write = new StreamWriter(write_accountNo);
+            write.WriteLine(number);
+
+            write.Close();
+            write_accountNo.Close();
+
+            #endregion            // v_autoAccountNumber = v_autoAccountNumber + 1;
+            //   AccNo = v_autoAccountNumber;
+
+
+        }
+        #endregion
+    }
+}
---
+++ Day 4/static_fileio_OOPS/static_fileio_OOPS/Program.cs
@@ -0,0 +1,100 @@
+﻿
+
+using static_fileio_OOPS;
+using System.Threading.Channels;
+using System.IO;
+
+
+#region Static
+//Accounts accObj = new Accounts("Nikhil", AccountTypes.Savings, 5000, true); // this has successfully created a new object
+
+//List<Accounts> accList = new List<Accounts>();
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Accounts accObj = new Accounts("Name " + i, AccountTypes.Savings, 5000,true);
+//    accList.Add(accObj);
+//}
+
+//foreach (var item in accList)
+//{
+//    Console.WriteLine("Account No : " + item.AccNo);
+//    Console.WriteLine("Account Name : " + item.AccName);
+//}
+#endregion
+
+#region File IO - Demo 1 
+
+//FileStream myFile = new FileStream("myintro.txt",FileMode.Create,FileAccess.Write); //book
+//StreamWriter pen = new StreamWriter(myFile); //pen
+
+//pen.WriteLine("Hello and welcome to my Demo on FileIO");
+//pen.WriteLine("my Name is Nikhil");
+//pen.WriteLine("I am a trainer");
+//pen.WriteLine("I am from Mumbai");
+//pen.WriteLine("Its raining heavy here");
+//pen.WriteLine("I love programming");
+
+//pen.Close();
+//myFile.Close();
+
+//Console.WriteLine("Details written to file");
+
+#endregion
+
+#region File IO - Demo 2
+
+//Console.WriteLine("Please Type your name and press enter");
+//string user = Console.ReadLine();
+
+//FileStream myFile = new FileStream(user + ".txt", FileMode.Create, FileAccess.Write);
+//StreamWriter pen = new StreamWriter(myFile);
+
+//Console.WriteLine("Provide all the information you wish to, and say Bye once done");
+
+//string lastword = "Not Bye";
+
+//while (lastword != "Bye")
+//{
+
+//    string content = Console.ReadLine();
+//    pen.WriteLine(content);
+
+//    if (content == "Bye")
+//    {
+//        lastword = "Bye";
+//    }
+//}
+
+//pen.Close();
+//myFile.Close();
+
+
+#endregion
+
+#region FileIO - Demo 3 - Read the file
+
+//Console.WriteLine("Please Enter Name to know the details");
+//string name = Console.ReadLine();
+
+//FileStream myFile = new FileStream(name + ".txt",FileMode.Open,FileAccess.Read);
+//StreamReader read = new StreamReader(myFile);
+
+//Console.WriteLine(read.ReadToEnd());
+
+//read.Close();
+//myFile.Close();
+
+#endregion
+
+
+#region FileIO - Demo 4 - Auto Account No generation
+
+Accounts accObj = new Accounts("Guest",AccountTypes.Savings,8000,true);
+
+Console.WriteLine("Account Created Successfully, New Account No is as below");
+Console.WriteLine("Account No : " + accObj.AccNo);
+
+
+
+#endregion
