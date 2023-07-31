2e92b095b5ebb8a8e9dbd043f7233898e47383a0
Nikhil <nikhilshah.net@hotmail.com>
2023-07-31 16:31:25 +0530

    ADO.net

---
+++ Day 5/ADO.Net_demo/ADO_net_demo/ADO_net_demo/AccountDetails.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Threading.Channels;
+
+namespace ADO_net_demo
+{
+    internal class AccountDetails
+    {
+        public int AccountNo { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public double AccountBalance { get; set; }
+        public bool AccountisActive { get; set; }
+
+        public string AddNewAccount(AccountDetails newAccount)
+        {
+            /* SqlConnection con = new SqlConnection("server=;database=;userid=;password=");*/ //for server authentication
+            SqlConnection con = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=bankingDB;integrated security=true");
+            SqlCommand cmd_insertAccount = new SqlCommand("insert into AccountDetails values(@accNo,@accName,@accType,@accBalance,@accIsActive)", con);
+
+            cmd_insertAccount.Parameters.AddWithValue("@accNo", newAccount.AccountNo);
+            cmd_insertAccount.Parameters.AddWithValue("@accName", newAccount.AccountName);
+            cmd_insertAccount.Parameters.AddWithValue("@accType", newAccount.AccountType);
+            cmd_insertAccount.Parameters.AddWithValue("@accBalance", newAccount.AccountBalance);
+            cmd_insertAccount.Parameters.AddWithValue("@accIsActive", newAccount.AccountisActive);
+
+            try
+            {
+                con.Open();
+                cmd_insertAccount.ExecuteNonQuery();
+                return "New Account Added Successfully";
+            }
+            catch (SqlException es)
+            {
+
+                throw new Exception(es.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public string DeleteAccount(int accNo)
+        {
+            SqlConnection con = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=bankingDB;integrated security=true");
+            SqlCommand cmd_DeleteAccount = new SqlCommand("delete from accountDetails where accNo =@accNo", con);
+            cmd_DeleteAccount.Parameters.AddWithValue("@accNo", accNo);
+
+            try
+            {
+                con.Open();
+                int delete = cmd_DeleteAccount.ExecuteNonQuery();
+                if (delete == 1)
+                {
+                    return "Account Delete Successfully";
+                }
+                else
+                {
+                    return "Account Not Found";
+                }
+            }
+            catch (SqlException es)
+            {
+
+                throw new Exception(es.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public string updateAccount(AccountDetails changes)
+        {
+            /* SqlConnection con = new SqlConnection("server=;database=;userid=;password=");*/ //for server authentication
+            SqlConnection con = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=bankingDB;integrated security=true");
+            SqlCommand cmd_updateAccount = new SqlCommand("update accountDetails set accName=@accName,accType=@accType,accBalance=@accBalance,accIsActive=@accIsActive where accNo=@accNo", con);
+
+            cmd_updateAccount.Parameters.AddWithValue("@accNo", changes.AccountName);
+            cmd_updateAccount.Parameters.AddWithValue("@accName", changes.AccountName);
+            cmd_updateAccount.Parameters.AddWithValue("@accType", changes.AccountType);
+            cmd_updateAccount.Parameters.AddWithValue("@accBalance", changes.AccountBalance);
+            cmd_updateAccount.Parameters.AddWithValue("@accIsActive", changes.AccountisActive);
+
+            try
+            {
+                con.Open();
+               int result =  cmd_updateAccount.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    return "Account Updated Successfully";
+                }
+                else
+                {
+                    return "Account Not Found";
+                }
+               
+            }
+            catch (SqlException es)
+            {
+
+                throw new Exception(es.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public AccountDetails ViewAccount(int accNo)
+        {
+            SqlConnection con = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=bankingDB;integrated security=true");
+            SqlCommand cmd_ViewAccount = new SqlCommand("select * from accountDetails where accNo=@accNo", con);
+
+            cmd_ViewAccount.Parameters.AddWithValue("@accNo", accNo);
+            SqlDataReader readAccount = null;
+
+            try
+            {
+                con.Open();
+                readAccount = cmd_ViewAccount.ExecuteReader();
+                if(readAccount.Read())
+                {
+                    AccountDetails acc = new AccountDetails();
+                    acc.AccountNo = Convert.ToInt32(readAccount[0]);
+                    acc.AccountName = readAccount[1].ToString();
+                    acc.AccountType = readAccount[2].ToString();
+                    acc.AccountBalance =Convert.ToDouble(readAccount[3]);
+                    acc.AccountisActive =Convert.ToBoolean(readAccount[4].ToString());
+                    return acc;
+                }
+                else
+                {
+                    throw new Exception("Account Not Found");
+                }
+
+            }
+            catch (SqlException es)
+            {
+                throw new Exception(es.Message);                
+            }
+            finally
+            {
+                readAccount.Close();
+                con.Close();
+            }
+        }
+    }
+}
---
+++ Day 5/ADO.Net_demo/ADO_net_demo/ADO_net_demo/Program.cs
@@ -0,0 +1,118 @@
+﻿using ADO_net_demo;
+using System.Threading.Channels;
+
+Console.WriteLine("!!! Welcome to Banking !!!");
+Console.WriteLine("1. Add New Account");
+Console.WriteLine("2. Delete Account");
+Console.WriteLine("3. Update Account");
+Console.WriteLine("4. View Account");
+
+
+AccountDetails accCRUD = new AccountDetails();
+
+AccountDetails accObj = new AccountDetails();
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch(choice)
+{
+	case 1:
+			#region Add New Account
+
+
+
+Console.WriteLine("Enter Account Number");
+accObj.AccountNo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Account Name");
+accObj.AccountName = Console.ReadLine();
+Console.WriteLine("Enter Account Type");
+accObj.AccountType = Console.ReadLine();
+
+Console.WriteLine("Enter Account Balance");
+accObj.AccountBalance = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Account Status");
+accObj.AccountisActive = Convert.ToBoolean(Console.ReadLine());
+
+string result = accCRUD.AddNewAccount(accObj);
+Console.WriteLine(result);
+		#endregion
+		break;
+	case 2:
+			#region Delete Account
+
+Console.WriteLine("Please Enter Account No");
+int accNo = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+	string deleteResult = accCRUD.DeleteAccount(accNo);
+	Console.WriteLine(deleteResult);
+}
+catch (Exception es)
+{
+	Console.WriteLine(es.Message);
+}
+
+#endregion
+		break;
+	case 3:
+			#region EditAccount
+
+
+Console.WriteLine("Enter Account Number to be updated");
+accObj.AccountNo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter New Account Name");
+accObj.AccountName = Console.ReadLine();
+Console.WriteLine("Enter New Account Type");
+accObj.AccountType = Console.ReadLine();
+
+Console.WriteLine("Enter New Account Balance");
+accObj.AccountBalance = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter New Account Status");
+accObj.AccountisActive = Convert.ToBoolean(Console.ReadLine());
+
+string updateResult = accCRUD.AddNewAccount(accObj);
+Console.WriteLine(updateResult);
+
+try
+{
+	string updateresult = accCRUD.updateAccount(accObj);
+	Console.WriteLine(updateResult);
+}
+catch (Exception es)
+{
+	Console.WriteLine(es.Message);
+	throw;
+}
+
+
+#endregion
+		break;
+	case 4:
+			#region View Account Details
+        Console.WriteLine("Please Enter Account No to View account Details");
+        int accViewDetails = Convert.ToInt32(Console.ReadLine());
+		try
+		{
+			accObj = accCRUD.ViewAccount(accViewDetails);
+
+			Console.WriteLine("Account Number : " + accObj.AccountNo);
+			Console.WriteLine("Account Name : " + accObj.AccountName);
+			Console.WriteLine("Account Balance : " + accObj.AccountBalance);
+			Console.WriteLine("Account Type : " + accObj.AccountType);
+			Console.WriteLine("Account Is Active : " + accObj.AccountisActive);
+		}
+		catch(Exception es)
+		{
+			Console.WriteLine(es.Message);
+		}
+		break;
+    #endregion
+
+  //  default:
+		//Console.WriteLine("Invalid Option");
+}
+
+
+
